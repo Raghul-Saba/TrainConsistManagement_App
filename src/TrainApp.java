fb8f312a5e0228ea5198c8def416f969743d7585
Raghul-Saba <rs2298@srmist.edu.in>
2026-03-24 11:11:26 +0530

    Base application setup

--- src/TrainApp.java
+++ src/TrainApp.java
@@ -1,3 +1,12 @@
-void main() {
-    
+import java.util.*;
+public class TrainConsistMgmnt {
+    static void main() {
+        System.out.println("==========================================");
+        System.out.println("   === Train Consist Management App ===   ");
+        System.out.println("==========================================");
+        List<String> trainConsist = new ArrayList<>();
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial Bogie Consist : "+trainConsist.size());
+        System.out.print("Current Train Consist : " + trainConsist);
+    }
 }
