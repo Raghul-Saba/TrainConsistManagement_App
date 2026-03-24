bc7bc03dac26fe5de4a79442af5a5c224131263e
Raghul-Saba <rs2298@srmist.edu.in>
2026-03-24 23:24:35 +0530

    Completed UC3

--- TrainConsistManagement_App/src/TrainConsistMgmnt.java
+++ TrainConsistManagement_App/src/TrainConsistMgmnt.java
@@ -38,6 +38,26 @@ public class TrainConsistMgmnt {
 
         System.out.println("\nCRUD operations(UC2) completed successfully...");
 
+        System.out.println("=======================================");
+        System.out.println("      UC3: Track Unique Bogie IDs      ");
+        System.out.println("=======================================");
+
+        Set<String> bogies = new HashSet<>();
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+
+        bogies.add("BG101");
+        bogies.add("BG102");
+
+        System.out.println("Bogie IDs After Insertion: ");
+        System.out.println(bogies);
+
+        System.out.println("\nNote:\nDuplicates are automatically ignored by the HashSet.\n");
+
+        System.out.println("Uniqueness validation completed... (UC3)");
+
 
     }
 }
