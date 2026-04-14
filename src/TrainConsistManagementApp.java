25ce4e0b96df2255e81319f3c4e2f35dfe6985a5
Raghul-Saba <rs2298@srmist.edu.in>
2026-04-14 21:42:42 +0530

    UC19 - Binary Search for Bogie ID (Optimized Searching)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -6,30 +6,56 @@ public class TrainConsistManagementApp {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] capacities = new int[n];
+        String[] bogieIds = new String[n];
 
         for (int i = 0; i < n; i++) {
-            capacities[i] = sc.nextInt();
+            bogieIds[i] = sc.next();
         }
 
-        bubbleSort(capacities);
+        String key = sc.next();
 
-        for (int i = 0; i < capacities.length; i++) {
-            System.out.print(capacities[i] + " ");
+        sortArray(bogieIds);
+
+        boolean found = binarySearch(bogieIds, key);
+
+        if (found) {
+            System.out.println("Bogie ID found");
+        } else {
+            System.out.println("Bogie ID not found");
         }
     }
 
-    public static void bubbleSort(int[] arr) {
+    public static void sortArray(String[] arr) {
         int n = arr.length;
-
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    String temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
         }
+    }
+
+    public static boolean binarySearch(String[] arr, String key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int cmp = arr[mid].compareTo(key);
+
+            if (cmp == 0) {
+                return true;
+            } else if (cmp < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
     }
 }
